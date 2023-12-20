86008881d19a26a28bb74fa17563a636f6661b91
Jimmy <jinshuaifu@gmail.com>
2023-12-20 14:39:40 +0800

    feat: a new component of ColorScale

---
+++ superset-frontend/plugins/legacy-preset-chart-deckgl/src/components/ColorBar.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable theme-colors/no-literal-colors */
+import React, { CSSProperties } from 'react';
+
+const calculateDistributionPoints = (min: number, max: number) => {
+  const points = [];
+  points.push(min);
+  points.push(min / 2);
+  points.push(0);
+  points.push(max / 2);
+  points.push(max);
+  return points.reverse();
+};
+
+const ColorBar = ({ gradientStops, max, min }: any) => {
+  const gradient = gradientStops
+    .map(
+      (stop: { color: string; value: number }) =>
+        `${stop.color} ${stop.value}%`,
+    )
+    .join(', ');
+  const distributionPoints = calculateDistributionPoints(min, max);
+  const style: { bar: CSSProperties; label: CSSProperties } = {
+    bar: {
+      position: 'absolute',
+      top: '50px',
+      left: '50px',
+      width: '20px', // Adjusted for vertical bar
+      height: '80%',
+      backgroundImage: `linear-gradient(to top, ${gradient})`, // Gradient now goes top to bottom
+      borderRadius: '5px',
+      border: '1px solid #000',
+    },
+    label: {
+      position: 'absolute',
+      left: '60px',
+      marginLeft: '12px',
+      fontSize: '12px',
+    },
+  };
+
+  return (
+    <div>
+      {distributionPoints.map((point, index) => (
+        <div
+          key={`point-${index}`}
+          style={{ ...style.label, top: `${19 * index + 8}%` }}
+        >
+          {point.toFixed(2)}
+        </div>
+      ))}
+      <div style={style.bar} />
+    </div>
+  );
+};
+export default ColorBar;
